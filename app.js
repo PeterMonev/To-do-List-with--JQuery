4cc465bcef975d3238b074405ca8906906d660cb
PeterMonev <monevpeter@gmail.com>
2023-08-01 14:47:44 +0300

    Creating checkbox functionality

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ $("#addBtn").click(function (event) {
 
 function addLocalStorage(task) {
   const id = new Date().getTime().toString();
-  let taskObj = { id, value: task};
+  let taskObj = { id, value: task, checked: false };
   let tasks = JSON.parse(localStorage.getItem("tasks"))
     ? JSON.parse(localStorage.getItem("tasks"))
     : [];
@@ -62,9 +62,11 @@ function createLi(task) {
          task.id
        }" id="list-item" class="list-item row justify-content-between pt-3 m-3 border border-light rounded-pill">
        <div class="col-1">${$("#todoList").children().length + 1}</div>
-       <div class="text-center col-5"><p>${task.value}</p></div>
+       <div class="text-center col-5"><p class="${
+         task.checked ? "text-decoration-line-through" : ""
+       }">${task.value}</p></div>
        <div class="col-6 justify-content-between">
-       <input id="chechbox" class="p-3" type="checkbox" >
+       <input id="checkbox" class="p-3" type="checkbox"  ${task.checked ? "checked" : ""}> 
        <button id="editBtn" class="button edit mx-5"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="lightsalmon" class="bi bi-pencil-square" viewBox="0 0 16 16">
        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -131,6 +133,21 @@ $("#editForm").submit(function (event) {
 
 // CHECHBOX FUNCTIONALITY
 
+$(document).on("click", "#checkbox", function () {
+  const id = $(this).closest("li").data("id");
+  const isChecked = $(this).is(":checked");
+
+  checkboxTask(id, isChecked);
+});
+
+function checkboxTask(id, isChecked) {
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  const taskIndex = tasks.findIndex((task) => task.id == id);
+  tasks[taskIndex].checked = isChecked;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  getLocalStorage();
+}
 
 // ALERT NOTIFICATION FUNCTIONALITY
 
